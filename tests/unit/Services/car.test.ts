20eec92ab3c8bd0338ad7177cfc53d0cde74fb13
Marcelo Carvalho <marcelo.carvalho2@outlook.com>
2023-02-07 21:04:39 -0300

    Tests /cars /motorcycles

---
+++ tests/unit/Services/car.test.ts
@@ -0,0 +1,163 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+
+describe('Verificar rota /cars', function () {
+  describe('POST Car', function () {
+    it('POST /cars', async function () {
+      const carInput: ICar = {
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.990,
+        doorsQty: 4,
+        seatsQty: 5,
+      };
+
+      const carOutput = {
+        id: '6348513f34c397abcad040b2',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.990,
+        doorsQty: 4,
+        seatsQty: 5,
+      };
+
+      sinon.stub(Model, 'create').resolves(carOutput);
+  
+      const service = new CarService();
+      const result = await service.create(carInput);
+  
+      expect(result).to.be.deep.equal(carOutput);
+    });
+  });
+
+  describe('Verificar GET cars', function () {
+    it('GET /cars', async function () {
+      const carOutput = [
+        {
+          _id: '634852326b35b59438fbea2f',
+          model: 'Marea',
+          year: 2002,
+          color: 'Black',
+          status: true,
+          buyValue: 15.99,
+          doorsQty: 4,
+          seatsQty: 5,
+        },
+        {
+          _id: '634852326b35b59438fbea31',
+          model: 'Tempra',
+          year: 1995,
+          color: 'Black',
+          status: false,
+          buyValue: 39,
+          doorsQty: 2,
+          seatsQty: 5,
+        },
+      ];
+
+      const serviceOutput = [
+        {
+          id: '634852326b35b59438fbea2f',
+          model: 'Marea',
+          year: 2002,
+          color: 'Black',
+          status: true,
+          buyValue: 15.99,
+          doorsQty: 4,
+          seatsQty: 5,
+        },
+        {
+          id: '634852326b35b59438fbea31',
+          model: 'Tempra',
+          year: 1995,
+          color: 'Black',
+          status: false,
+          buyValue: 39,
+          doorsQty: 2,
+          seatsQty: 5,
+        },
+      ];
+
+      sinon.stub(Model, 'find').resolves(carOutput);
+  
+      const service = new CarService();
+      const result = await service.getAll();
+  
+      expect(result).to.be.deep.equal(serviceOutput);
+    });
+  });
+
+  describe('Verificar GET cars:id', function () {
+    it('GET /cars/:id', async function () {
+      const carOutput = {
+        _id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      };
+
+      const serviceOutput = {
+        id: '634852326b35b59438fbea2f',
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        status: true,
+        buyValue: 15.99,
+        doorsQty: 4,
+        seatsQty: 5,
+      };
+  
+      sinon.stub(Model, 'findById').resolves(carOutput);
+  
+      const service = new CarService();
+      const result = await service.getById('634852326b35b59438fbea2f');
+
+      expect(result).to.be.deep.equal(serviceOutput);
+    });
+
+    it('com id inexistente', async function () {  
+      sinon.stub(Model, 'findById').resolves([]);
+
+      const service = new CarService();
+      const result = await service.getById('12345634852326b35b59438f');
+
+      expect(result?.id).to.be.deep.equal(undefined);
+    });
+  });
+
+  describe('Verificar PUT /cars', function () {
+    it('PUT /cars/:id', async function () {
+      const carUpdated = {
+        model: 'Marea',
+        year: 1992,
+        color: 'Red',
+        status: true,
+        buyValue: 12.000,
+        doorsQty: 2,
+        seatsQty: 5,
+      };
+
+      sinon.stub(Model, 'updateOne').resolves();
+
+      const service = new CarService();
+      const result = await service.updateCar('634852326b35b59438fbea2f', carUpdated);
+
+      expect(result).to.be.deep.equal(null);
+    });
+  });
+  
+  afterEach(function () {
+    sinon.restore();
+  });
+});
---
+++ tests/unit/Services/motorcycle.test.ts
@@ -0,0 +1,163 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+
+describe('Verificar rota /motorcycles', function () {
+  describe('POST /Motorcycle', function () {
+    it('POST /motorcycles', async function () {
+      const motorcycleInput: IMotorcycle = {
+        model: 'Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+
+      const motorcycleOutput = {
+        id: '6348513f34c397abcad040b2',
+        model: 'Hornet',
+        year: 2005,
+        color: 'Yellow',
+        status: true,
+        buyValue: 30.000,
+        category: 'Street',
+        engineCapacity: 600,
+      };
+
+      sinon.stub(Model, 'create').resolves(motorcycleOutput);
+  
+      const service = new MotorcycleService();
+      const result = await service.create(motorcycleInput);
+  
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
+  });
+
+  describe('Verificar GET motorcycle', function () {
+    it('GET /motorcycles', async function () {
+      const motorcycleOutput = [
+        {
+          _id: '634852326b35b59438fbea2f',
+          model: 'Honda Cb 600f Hornet',
+          year: 2005,
+          color: 'Yellow',
+          status: true,
+          buyValue: 30.000,
+          category: 'Street',
+          engineCapacity: 600,
+        },
+        {
+          _id: '634852326b35b59438fbea31',
+          model: 'Honda Cbr 1000rr',
+          year: 2011,
+          color: 'Orange',
+          status: true,
+          buyValue: 59.900,
+          category: 'Street',
+          engineCapacity: 1000,
+        },
+      ];
+
+      const serviceOutput = [
+        {
+          id: '634852326b35b59438fbea2f',
+          model: 'Honda Cb 600f Hornet',
+          year: 2005,
+          color: 'Yellow',
+          status: true,
+          buyValue: 30.000,
+          category: 'Street',
+          engineCapacity: 600,
+        },
+        {
+          id: '634852326b35b59438fbea31',
+          model: 'Honda Cbr 1000rr',
+          year: 2011,
+          color: 'Orange',
+          status: true,
+          buyValue: 59.900,
+          category: 'Street',
+          engineCapacity: 1000,
+        },
+      ];
+
+      sinon.stub(Model, 'find').resolves(motorcycleOutput);
+  
+      const service = new MotorcycleService();
+      const result = await service.getAll();
+  
+      expect(result).to.be.deep.equal(serviceOutput);
+    });
+  });
+
+  describe('Verificar GET motorcycle:id', function () {
+    it('GET /motorcycles/:id', async function () {
+      const motorcycleOutput = {
+        _id: '634852326b35b59438fbea31',
+        model: 'Cbr 1000rr',
+        year: 2011,
+        color: 'Orange',
+        status: true,
+        buyValue: 59.900,
+        category: 'Street',
+        engineCapacity: 1000,
+      };
+
+      const serviceOutput = {
+        id: '634852326b35b59438fbea31',
+        model: 'Cbr 1000rr',
+        year: 2011,
+        color: 'Orange',
+        status: true,
+        buyValue: 59.900,
+        category: 'Street',
+        engineCapacity: 1000,
+      };
+  
+      sinon.stub(Model, 'findById').resolves(motorcycleOutput);
+  
+      const service = new MotorcycleService();
+      const result = await service.getById('634852326b35b59438fbea31');
+
+      expect(result).to.be.deep.equal(serviceOutput);
+    });
+    
+    it('com id inexistente', async function () {  
+      sinon.stub(Model, 'findById').resolves([]);
+
+      const service = new MotorcycleService();
+      const result = await service.getById('12345634852326b35b59438f');
+
+      expect(result?.id).to.be.deep.equal(undefined);
+    });
+  });
+
+  describe('Verificar PUT /motorcycles', function () {
+    it('PUT /motorcycles/:id', async function () {
+      const motorcycleUpdated = {
+        model: 'Honda',
+        year: 2014,
+        color: 'Red',
+        status: true,
+        buyValue: 45.000,
+        category: 'Street',
+        engineCapacity: 6000,
+      };
+
+      sinon.stub(Model, 'updateOne').resolves();
+
+      const service = new MotorcycleService();
+      const result = await service.updateMotorcycle('634852326b35b59438fbea31', motorcycleUpdated);
+
+      expect(result).to.be.deep.equal(null);
+    });
+  });
+  
+  afterEach(function () {
+    sinon.restore();
+  });
+});
